2b936b1feebffb7841e5ad81bde67dcb797cb91e
Igor Rocha Lima <contatoigorrochalima@hotmail.com>
2023-11-14 12:30:41 -0300

    Up on github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { HelloComponent } from './components/hello/hello.component';
+import { StacksComponent } from './components/stacks/stacks.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    HelloComponent,
+    StacksComponent,
+    ExperienceComponent,
+    ContactComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
