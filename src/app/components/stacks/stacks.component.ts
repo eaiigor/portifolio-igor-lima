10f9eb73fc322f67c4ddbad426acd6e9f7dbd23c
Igor Rocha Lima <contatoigorrochalima@hotmail.com>
2023-11-20 18:26:17 -0300

    initial stacks page

--- src/app/components/stacks/stacks.component.ts
+++ src/app/components/stacks/stacks.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-stacks',
   templateUrl: './stacks.component.html',
   styleUrls: ['./stacks.component.scss']
 })
-export class StacksComponent {
+export class StacksComponent implements OnInit {
+
+  ngOnInit(): void {
+    const icons = document.querySelectorAll('.fs-32');
+    const description = document.getElementById('description');
+
+    icons.forEach(icon => {
+      icon.addEventListener('mouseover', () => {
+        const iconDescription = icon.getAttribute('data-description');
+        if (description) {
+          description.textContent = iconDescription || '';
+        }
+      });
+
+      icon.addEventListener('mouseout', () => {
+        if (description) {
+          description.textContent = '*passe o cursor do mouse no card para ler*';
+        }
+      });
+    });
+  }
 
 }
